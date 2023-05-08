30817a4665923ff028012641b3ad62ec1eea45a0
Nasser Abunasir <79524536+Nasser-96@users.noreply.github.com>
2023-05-08 22:15:34 +0300

    delete the height from Home component

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -6,7 +6,7 @@ export default function Home({}: Props)
 {
   return (
     <div className={`font-courier`}>
-      <div className={`h-[1000px]`}>
+      <div className={``}>
       </div>
     </div>
   )
